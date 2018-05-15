13e378f25988c24dea92aba21dd715f1ed749fa9
Harald Floor Wilhelmsen <haraldfw@users.noreply.github.com>
2018-05-15 14:13:25 +0200

    Fixed newline at end of url-token

--- src/catalyst.py
+++ src/catalyst.py
@@ -26,7 +26,7 @@ def init():
 def get_from_file(path):
     try:
         with open(path, 'r') as f:
-            return f.read()
+            return f.read().strip()
     except IOError:
         return None
 
